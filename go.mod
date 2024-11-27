ccc26cff434079ec9dae0465f166324bcbfa864b
Jacob Dobson <jacob.dobson@daugherty.com>
2024-11-27 14:12:16 -0600

    Stayin' Alive

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dobsonsystemsdesign/maurice
 
 go 1.23.3
+
+require github.com/micmonay/keybd_event v1.1.2
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"runtime"
+	"time"
+
+	"github.com/micmonay/keybd_event"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+		panic(err)
+	}
+
+	if runtime.GOOS == "linux" {
+		time.Sleep(2 * time.Second)
+	}
+
+	kb.SetKeys(keybd_event.VK_F20)
+
+	for {
+		pressKey(kb)
+		time.Sleep(90 * time.Second)
+	}
+}
+
+func pressKey(kb keybd_event.KeyBonding) {
+	kb.Press()
+	time.Sleep(10 * time.Millisecond)
+	kb.Release()
 }
